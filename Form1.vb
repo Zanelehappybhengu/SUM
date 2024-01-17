10510b596f3e0ecb285f1c1c0eba01fc6e688ff1
Dell-User <Dell-User@DESKTOP-S7HKJ5C>
2024-01-17 16:06:00 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FIRSTNUMBER, SECONDNUMBER As Integer
+        FIRSTNUMBER = Val(TextBox1.Text)
+        SECONDNUMBER = Val(TextBox2.Text)
+
+        SUM(FIRSTNUMBER, SECONDNUMBER)
+
+    End Sub
+    Sub sum(a As Integer, b As Integer)
+        Dim total As Integer
+        total = a + b
+        TextBox3.Text = total
+
+    End Sub
+End Class
